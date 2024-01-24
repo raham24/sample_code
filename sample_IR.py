e5e4f9641fec7828d4fb6d912e78ace0f81232c9
Raham <99924678+raham24@users.noreply.github.com>
2024-01-24 11:17:26 -0500

    Added AlphaBot2.py

--- sample_IR.py
+++ sample_IR.py
@@ -12,8 +12,9 @@ GPIO.setwarnings(False)
 GPIO.setup(DR,GPIO.IN,GPIO.PUD_UP) # set Infrared sensor as input, type PUD_UP (pull up resistor)
 GPIO.setup(DL,GPIO.IN,GPIO.PUD_UP)
 
-Ab.setPWMA(20) # set duty cycle for the left motor
-Ab.setPWMB(20) # set duty cycle for the right motor
+Ab.setPWMA(20)
+Ab.setPWMB(20)
 
+# set duty cycle for the left motor 
 # Duty cycle is used to set the speed of the robot
 # Recomended duty cycle is anywhere between 20-30
